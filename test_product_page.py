d9bdf4bc96a5b4e5ead5266a20e28d77d743d422
iRatatuii <andreikarpenkin123@gmail.com>
2022-05-23 15:24:38 +0300

    add func guest_should_see_login_link and guest_should_see_login_page

--- test_product_page.py
+++ test_product_page.py
@@ -26,6 +26,7 @@ def test_guest_can_add_to_basket(browser, links):  # links - добавить в
     page.check_price_and_title_book()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
     page = ProductPage(browser, link)
@@ -34,6 +35,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
     page = ProductPage(browser, link)
@@ -41,9 +43,24 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket_and_click()
     page.should_dissapear_of_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
